b9550916b9cf5e8e8936d0aa16bb1525b1ff3a76
SokolOFFF <egor1sokolov@gmail.com>
2023-06-22 16:22:57 +0300

    created folder for backend functions and initialized the file

---
+++ backend/backend_functions.py
@@ -0,0 +1,20 @@
+# TODO: implement
+def get_rated_books(user_id):
+    # take data from database
+    # return data
+    pass
+
+
+# TODO: implement
+def get_recommendations(user_id, number_of_books):
+    # recommendations = function(user_id, number_of_books)
+    # add data of books to recommendations
+    # return books
+    pass
+
+
+# TODO: implement
+def search_book(request):
+    # search books in database
+    # return books
+    pass
