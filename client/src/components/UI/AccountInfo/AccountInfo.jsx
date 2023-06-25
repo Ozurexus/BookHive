8d09920fe34beaca33c5933386f2b2f6791f3119
Marat <freiden2004@gmail.com>
2023-06-26 00:30:52 +0300

    MyBooks page was upgraded

--- client/src/components/UI/AccountInfo/AccountInfo.jsx
+++ client/src/components/UI/AccountInfo/AccountInfo.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import {Avatar} from "@mui/material";
+import style from "../../../styles/AccountInfo.module.css";
+
 
 function AccountInfo(props) {
-    return (
-        <div>
-            <p>Login: </p>
-            <p>Books reviewed</p>
-            <a>Change the password</a>
+    let name = "Marat Bulatov";
+    let booksreviewed = 5;
+    let status = "Cool guy";
+    return (<div className={style.accountInfo}>
+            <div className={style.accountInfoWithoutStatus}>
+                <div className={style.avatar}>
+                    <Avatar alt="Remy Sharp" src="../Search/logo512.png" sx={{width: 140, height: 140}}/>
+                </div>
+                <div className={style.info}>
+                    <div className={style.name}>
+                        <p>{name}</p>
+                    </div>
+                    <div className={style.n_reviewed}>
+                        <p>Books reviewed: {booksreviewed}</p>
+                    </div>
+                </div>
+            </div>
+            <div className={style.status}>
+                <p>Status: {status}</p>
+            </div>
+            <div className={style.chPassBtn}>
+                <button>Change password</button>
+            </div>
         </div>
+
     );
 }
 
--- client/src/pages/MyBooks.jsx
+++ client/src/pages/MyBooks.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Navbar from "../components/UI/Navbar/Navbar";
 import AccountInfo from "../components/UI/AccountInfo/AccountInfo";
-
+import style from "../styles/MyBooks.module.css"
 
 const MyBooks = () => {
     return(
-        <div className={'page'}>
+        <div className={"page"}>
             <Navbar/>
-            <AccountInfo/>
+            <div className={style.container}>
+                <div className={style.leftpanel}>
+                    <div className={style.account}><AccountInfo/></div>
+                    <div className={style.otherOptions}>
+                        <button className={style.myRevBtn}>My reviews</button>
+                        <button className={style.wantToReadBtn}>Wants to read</button>
+                    </div>
+                </div>
+                <div className={style.booksgrid}></div>
+            </div>
         </div>
     )
 }
