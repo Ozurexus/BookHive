713f74490c4444fd7b355f1511a490146f3d6d36
Marat <freiden2004@gmail.com>
2023-06-27 01:01:06 +0300

    MyBooks page was upgraded

--- client/src/components/UI/BooksGrid/BooksGrid.jsx
+++ client/src/components/UI/BooksGrid/BooksGrid.jsx
@@ -1,16 +1,22 @@
 import React, {useContext} from 'react';
 import {UserContext} from "../../../context";
-
+import EmptyCover from "../EmptyCover/EmptyCover";
+import style from "./BooksGrid.module.css"
 function BooksGrid() {
     const {books} = useContext(UserContext);
     console.log(books);
-
+    const coverExist = (url) => {
+        const rand = Boolean(Math.round(Math.random()));
+        return rand;
+    }
     return (
-        <div style={{display:'flex', flexWrap:'wrap'}}>
+        <div className={style.grid}>
             {books.map((book) => (
-                <div key={book.id} style={{padding:'5px'}}>
-                    <img src={book.image_url_m} alt={book.title}/>
-                    <p>{book.title}</p>
+                <div key={book.id} className={style.gridItem}>
+                    {coverExist(book.image_url_m)
+                    ? <img src={book.image_url_m} alt={book.title}/>
+                    : <EmptyCover name={book.title}/>
+                    }
                 </div>
             ))}
         </div>
